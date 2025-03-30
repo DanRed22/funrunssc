a76685b14b7ce9764f951cca0a3c489c71ac5aed
DanRed22 <angeloushatchitsolutions@gmail.com>
2025-03-30 15:00:17 +0800

    Add MechanicsTreasure component and enhance Mechanics with detailed instructions

--- components/Mechanics.jsx
+++ components/Mechanics.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import TextContainer from './TextContainer';
 
 export default function Mechanics() {
+  const mechanics = [
+    '1. Participants  must track their progress via the Strava application to track their distance traveled and accumulate their total from March 31 to April 24. ',
+    '2. Participants may complete their kilometer bracket at their own pace, location, and preferred time. Once completed, you may submit your final distance through the official submission form.',
+    '3. The Treasure Hunt is a fun way to progress towards their running goal while searching for prizes. ',
+    '4. Top 3 finishers will win cash prizes. All participants shall receive an e-certificate for participating on the event. This will be sent via the email provided during registration',
+  ];
   return (
     <div
       id="mechanics
     "
-      className="h-[30rem] flex flex-col items-center justify-center bg-[#f0efa7] text-black text-2xl font-bold w-full"
+      className="p-12 !w-full flex flex-col items-center justify-center bg-[#f0efa7] text-black text-2xl font-bold"
     >
-      This site is a work in progress... Please check back later for updates!
+      <div className="nes-container with-title is-centered flex flex-col items-center justify-center !p-8">
+        <p className="text-5xl text-center !mb-8">Virtual Run Mechanics</p>
+        {mechanics.map((item, index) => (
+          <TextContainer
+            key={index}
+            message={item}
+            className={'my-4 border-black w-[80%] text-start p-4'}
+          />
+        ))}
+      </div>
     </div>
   );
 }
